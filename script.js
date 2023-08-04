e3ae7de8e32f5212d2d6d03ab7fea5ee6e081d74
Corruption38 <135686099+Corruption38@users.noreply.github.com>
2023-08-03 23:33:14 -0500

    Update and rename server.js to script.js

---
+++ script.js
@@ -0,0 +1,32 @@
+const webhookUrl = 'https://webhook.site/92f623f6-9a2f-4e30-b81b-f03b5558f4ef';
+const imageElement = document.getElementById('image');
+
+async function fetchImage() {
+  try {
+    const response = await fetch('https://your-image-source.com/api?keyword=chunie');
+    const data = await response.json();
+
+    if (data.imageUrl) {
+      imageElement.src = data.imageUrl;
+      sendToWebhook(data.imageUrl);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function sendToWebhook(imageUrl) {
+  try {
+    await fetch(webhookUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ imageUrl }),
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+fetchImage();
