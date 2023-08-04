7f5e45234470437a9284e259e4aed711d9f9d439
Corruption38 <135686099+Corruption38@users.noreply.github.com>
2023-08-03 23:49:11 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
 // script.js
 const fetchButton = document.getElementById('fetchButton');
 const imageContainer = document.getElementById('imageContainer');
-const webhookURL = 'https://92f623f6-9a2f-4e30-b81b-f03b5558f4ef@email.webhook.site';
+const serverURL = 'http://localhost:3000/send-email'; // Change to your server's URL
 const searchKeyword = 'chunie';
 
 fetchButton.addEventListener('click', async () => {
-    try {
-        const response = await fetch(`https://e621.net/search/posts.json?tags=${searchKeyword}`);
-        const data = await response.json();
-        const imageUrl = data.posts[0].file.url;
+  try {
+    const response = await fetch(`https://e621.net/search/posts.json?tags=${searchKeyword}`);
+    const data = await response.json();
+    const imageUrl = data.posts[0].file.url;
 
-        const image = document.createElement('img');
-        image.src = imageUrl;
-        imageContainer.innerHTML = '';
-        imageContainer.appendChild(image);
+    const image = document.createElement('img');
+    image.src = imageUrl;
+    imageContainer.innerHTML = '';
+    imageContainer.appendChild(image);
 
-        // Send image URL and message to the webhook
-        const webhookData = {
-            imageUrl,
-            message: 'test'
-        };
+    // Send image URL and message to the server
+    const webhookData = {
+      imageUrl,
+      message: 'test'
+    };
 
-        await fetch(webhookURL, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(webhookData),
-        });
-    } catch (error) {
-        console.error('An error occurred:', error);
-    }
+    await fetch(serverURL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(webhookData),
+    });
+  } catch (error) {
+    console.error('An error occurred:', error);
+  }
 });
